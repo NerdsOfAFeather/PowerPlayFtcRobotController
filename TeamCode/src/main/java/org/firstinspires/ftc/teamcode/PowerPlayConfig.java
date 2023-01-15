9faef8061aae6a32a58bee9fc78f731143f7bd73
NerdsOfAFeather <ftcteam18218@gmail.com>
2023-01-15 15:10:02 -0500

    Main Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayConfig.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="PowerPlayConfig", group="Linear Opmode")
+@Disabled
+public class PowerPlayConfig extends TensorFlowObjectDetectionWebcam {
+
+    public DcMotor leftFrontDrive = null;
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor rightBackDrive = null;
+
+
+    public void initDriveHardware() {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "FrontLeftDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "BackLeftDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "FrontRightDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "BackRightDrive");
+
+        // Most robots need the motors on one side to be reversed to drive forward.
+        // When you first test your robot, push the left joystick forward
+        // and flip the direction ( FORWARD <-> REVERSE ) of any wheel that runs backwards
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRightAuto4pt.java
@@ -0,0 +1,228 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file illustrates the concept of driving a path based on time.
+ * The code is structured as a LinearOpMode
+ *
+ * The code assumes that you do NOT have encoders on the wheels,
+ * otherwise you would use: RobotAutoDriveByEncoder;
+ *
+ * The desired path in this example is:
+ * - Drive forward for 3 seconds
+ * - Spin right for 1.3 seconds
+ * - Drive Backward for 1 Second
+ *
+ * The code is written in a simple form with no optimizations.
+ * However, there are several ways that this type of sequence could be
+ * streamlined,
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code
+ * folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver
+ * Station OpMode list
+ */
+
+@Autonomous(name = "RedRightAuto4pt ", group = "Robot")
+public class RedRightAuto4pt extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    public DcMotor leftFrontDrive = null;
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor rightBackDrive = null;
+    public DcMotor liftLiftMotor = null;
+    public Servo rightClawServo = null;
+    public Servo leftClawServo = null;
+
+    static final double FORWARD_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the drive system variables.
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "FrontLeftDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "BackLeftDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "FrontRightDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "BackRightDrive");
+        liftLiftMotor = hardwareMap.get(DcMotor.class, "LiftLiftMotor");
+        rightClawServo = hardwareMap.get(Servo.class, "RightClawServo");
+        leftClawServo = hardwareMap.get(Servo.class, "LeftClawServo");
+
+        // To drive forward, most robots need the motor on one side to be reversed,
+        // because the axles point in opposite directions.
+        // When run, this OpMode should start both motors driving forward. So adjust
+        // these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels. Gear
+        // Reduction or 90 Deg drives may require direction flips
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run"); //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path, ensuring that the Auto mode has not been
+        // stopped along the way
+
+
+			
+        clampClose();
+		liftUp(4);
+		driveForward(0.25);
+        clampOpen();
+        driveBackwards(0.15);
+		liftDown(1.5);
+
+		
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(500);
+    }
+
+    public void driveForward(double seconds) {
+        leftFrontDrive.setPower(FORWARD_SPEED);
+        leftBackDrive.setPower(FORWARD_SPEED);
+        rightFrontDrive.setPower(FORWARD_SPEED);
+        rightBackDrive.setPower(FORWARD_SPEED);
+        sleep((long) (seconds * 1000));
+		leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+		sleep(250);
+    }
+
+    public void driveBackwards(double seconds) {
+        leftFrontDrive.setPower(-FORWARD_SPEED);
+        leftBackDrive.setPower(-FORWARD_SPEED);
+        rightFrontDrive.setPower(-FORWARD_SPEED);
+        rightBackDrive.setPower(-FORWARD_SPEED);
+        sleep((long) (seconds * 1000));
+		leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+		sleep(250);
+    }
+
+    public void driveRight(double seconds) {
+        leftFrontDrive.setPower(FORWARD_SPEED);
+        leftBackDrive.setPower(-FORWARD_SPEED);
+        rightFrontDrive.setPower(-FORWARD_SPEED);
+        rightBackDrive.setPower(FORWARD_SPEED);
+        sleep((long) (seconds * 1000));
+		leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+		sleep(250);
+    }
+	
+	public void driveLeft(double seconds) {
+        leftFrontDrive.setPower(-FORWARD_SPEED);
+        leftBackDrive.setPower(FORWARD_SPEED);
+        rightFrontDrive.setPower(FORWARD_SPEED);
+        rightBackDrive.setPower(-FORWARD_SPEED);
+        sleep((long) (seconds * 1000));
+		leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+		sleep(250);
+    }
+
+    public void turnRight(double seconds) {
+        leftFrontDrive.setPower(FORWARD_SPEED);
+        leftBackDrive.setPower(FORWARD_SPEED);
+        rightFrontDrive.setPower(-FORWARD_SPEED);
+        rightBackDrive.setPower(-FORWARD_SPEED);
+        sleep((long) (seconds * 1000));
+		leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+		sleep(250);
+    }
+	
+	public void turnLeft(double seconds) {
+        leftFrontDrive.setPower(-FORWARD_SPEED);
+        leftBackDrive.setPower(-FORWARD_SPEED);
+        rightFrontDrive.setPower(FORWARD_SPEED);
+        rightBackDrive.setPower(FORWARD_SPEED);
+        sleep((long) (seconds * 1000));
+		leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+		sleep(250);
+    }
+	
+	public void liftUp(double seconds){
+		liftLiftMotor.setPower(0.5);
+		sleep((long) (seconds * 1000));
+		liftLiftMotor.setPower(0);
+		sleep(250);
+	}
+	
+	public void liftDown(double seconds){
+		liftLiftMotor.setPower(-0.5);
+		sleep((long) (seconds * 1000));
+		liftLiftMotor.setPower(0);
+		sleep(250);
+	}
+	
+	public void clampClose(){
+		leftClawServo.setPosition(0.0);
+        rightClawServo.setPosition(1.0);
+        sleep(250);
+	}
+	
+	public void clampOpen(){
+		leftClawServo.setPosition(1.0);
+        rightClawServo.setPosition(0.0);
+        sleep(250);
+	}
+}
